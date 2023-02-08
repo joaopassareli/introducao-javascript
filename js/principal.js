fc568364761dfcf52da0e1e548d61b63ebccdf34
João Passareli <joaopassareli@gmail.com>
2023-02-07 21:23:44 -0300

    Fazendo algumas modificações no arquivo principal.js e index.css

--- js/principal.js
+++ js/principal.js
@@ -1,6 +1,20 @@
+var pacientes = document.querySelectorAll(".paciente");
 
-var title = document.querySelector(".title");
-console.log(title);
-console.log(title.textContent);
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-title.textContent = 'Bruna Nutricionista';
+    var peso = paciente.querySelector(".info-peso").textContent;
+
+    var altura = paciente.querySelector(".info-altura").textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    if ((peso <= 0 || peso >= 1000) || (altura <= 0 || altura >= 3.00)) {
+        alert("Peso ou altura inválidos");
+        tdImc.textContent = '-';
+        paciente.classList.add("paciente-invalido");
+    } else {
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
+}
